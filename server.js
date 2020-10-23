5bf14cf1051403cb4d5776a85e8b65b7685463cd
gbronca <42703789+gbronca@users.noreply.github.com>
2020-10-23 20:32:11 +0100

    Moved register and signin to separate files

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 const { response } = require('express');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 
 const db = knex({
 	client: 'pg',
@@ -27,53 +29,12 @@ app.get('/', (req, res) => {
 
 // Sign-in
 app.post('/signin', (req, res) => {
-	db.select('email', 'hash').from('login')
-		.where('email', '=', req.body.email)
-		.then(data => {
-			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-			if (isValid) {
-				return db.select('*').from('users')
-					.where('email', '=', req.body.email)
-					.then(user => {
-						res.json(user[0]);
-					})
-					.catch(err => res.status(400).json('unable to get user'));
-			} else {
-				res.status(400).json('Wrong username or password');
-			}
-		})
-		.catch(err => res.status(400).json('Wrong username or password'));
+	signin.handleSignin(req, res, db, bcrypt);
 });
 
 // Register
 app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
-	const salt = bcrypt.genSaltSync(10);
-	const hash = bcrypt.hashSync(password, salt);
-
-	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		})
-			.into('login')
-			.returning('email')
-			.then(loginEmail => {
-				return trx('users')
-					.returning('*')
-					.insert({
-						email: loginEmail[0],
-						name: name,
-						joined: new Date()
-					})
-					.then(user => {
-						res.json(user[0]);
-					});
-			})
-			.then(trx.commit)
-			.catch(trx.rollback)
-	})
-		.catch(err => res.status(400).json('unable to register'));
+	register.handleRegister(req, res, db, bcrypt);
 });
 
 // Profile
