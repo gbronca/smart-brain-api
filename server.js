200e30925b7fff51f37c2ddf2453103dc0209063
gbronca <42703789+gbronca@users.noreply.github.com>
2020-10-23 20:38:47 +0100

    Moved profile into a separate file in controllers folder

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const knex = require('knex');
 const { response } = require('express');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
 
 const db = knex({
 	client: 'pg',
@@ -38,18 +39,7 @@ app.post('/register', (req, res) => {
 });
 
 // Profile
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	db.select('*').from('users').where('id', id)
-		.then(user => {
-			if (user.length) {
-				res.json(user[0]);
-			} else {
-				res.status(400).json('Not found');
-			}
-		})
-		.catch(err => res.status(400).json('error getting user'));
-});
+app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db));
 
 // Image
 app.put('/image', (req, res) => {
